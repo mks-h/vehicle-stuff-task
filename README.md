c83fdb26a05311ef91d360cc384b516e43a8ac91
Maksym Hazevych <mhazevych@proton.me>
2024-09-26 19:01:32 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Car Dealer App
+
+To start working on the project, run the dev server:
+
+```bash
+pnpm dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the webpage.
---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+	return (
+		<div
+			className="w-screen h-screen
+		flex justify-center items-center
+		bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]"
+		>
+			<div
+				className="w-2/3 h-1/3 flex justify-center items-center
+			bg-white border-2 border-slate-50 drop-shadow-lg"
+			>
+				<h1 className="text-4xl font-bold">Car Dealer App</h1>
+			</div>
+		</div>
+	);
+}
